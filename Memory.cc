292f8d9b95d5636b1e01e5ba6eba6ac27e50025d
Jon Mandrell <jon.mandrell@gmail.com>
2019-12-29 11:31:07 -0800

    Added code to avoid accessing the memory file more than necessary.

--- Memory.cc
+++ Memory.cc
@@ -35,26 +35,38 @@ void Memory::WriteMem(unsigned int address, unsigned char value) {
 
 
 void Memory::Update() {
+	static unsigned int lastReadAddress = 0x10000;
+	static unsigned int lastReadValue = 0x10000;
+	static unsigned int lastWriteAddress = 0x10000;
+	static unsigned int lastWriteValue = 0x10000;
 	if (enable.GetOutput()) {
 		// they are reading memory, get the address from the address bus
 		unsigned int address = addressBus->GetValue();
 		unsigned char value = ReadMem(address);
-		for (unsigned int i = 0; i < 8; ++i) {
-			outputData.bits[i].Force((value & 1) != 0);
-			value >>= 1;
+		if (lastReadAddress != address || value != lastReadValue) {
+			lastReadAddress = address;
+			lastReadValue = value;
+			for (unsigned int i = 0; i < 8; ++i) {
+				outputData.bits[i].Force((value & 1) != 0);
+				value >>= 1;
+			}
 		}
 	} else {
 		// turn off the outputs
 		for (unsigned int i = 0; i < 8; ++i) {
 			outputData.bits[i].Force(false);
 		}
+		lastReadAddress = 0x10000;
 	}
 	if (writeSignal.GetOutput()) {
 		// they are writing memory, get the address from the address bus
 		unsigned int address = addressBus->GetValue();
 		// get the data from the data bus
 		unsigned char value = dataBus->GetValue();
-		std::cerr << address << " " << (unsigned int)value << std::endl;
-		WriteMem(address, value);
+		if (address != lastWriteAddress || value != lastWriteValue) {
+			lastWriteAddress = address;
+			lastWriteValue = value;
+			WriteMem(address, value);
+		}
 	}
 }
